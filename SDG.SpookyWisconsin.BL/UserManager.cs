55954278467233ce16b5e5bb244dfdbe10f15278
Colby Witthuhn <700228722@fvtc.edu>
2023-10-02 13:09:43 -0500

    Connected the UserManager to the database. Created UserManager methods (Login, GetHash, Seed).

--- SDG.SpookyWisconsin.BL/UserManager.cs
+++ SDG.SpookyWisconsin.BL/UserManager.cs
@@ -1,21 +1,123 @@
-﻿using SDG.SpookyWisconsin.BL.Models;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using SDG.SpookyWisconsin.BL.Models;
 using SDG.SpookyWisconsin.PL;
-using Microsoft.EntityFrameworkCore.Storage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
+using SDG.SpookyWisconsin.PL.Entities;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using System.Xml;
-
 
 namespace SDG.SpookyWisconsin.BL
 {
     public class UserManager
     {
         private const string NOTFOUND_MESSAGE = "Row does not exist";
+
+        public class LoginFailureException : Exception
+        {
+            public LoginFailureException() : base("Login Failure") { }
+            public LoginFailureException(string message) : base(message) { }
+        }
+
+        private static string GetHash(string password)
+        {
+            using (var hasher = SHA1.Create())
+            {
+                var hashbytes = Encoding.UTF8.GetBytes(password);
+                return Convert.ToBase64String(hasher.ComputeHash(hashbytes));
+            }
+        }
+        public static void Seed()
+        {
+            //seed is used like default data
+            using (SpookyWisconsinEntities dc = new SpookyWisconsinEntities())
+            {
+                if (!dc.tblUsers.Any())
+                {
+                    User user = new User
+                    {
+                        UserName = "cwitthuhn",
+                        Password = "colbypassword"
+                    };
+                    Insert(user);
+                    user = new User
+                    {
+                        UserName = "tlee",
+                        Password = "thaypassword"
+                    };
+                    Insert(user);
+                    user = new User
+                    {
+                        UserName = "cmarohl",
+                        Password = "cadinpassword"
+                    };
+                    Insert(user);
+                    user = new User
+                    {
+                        UserName = "cperez",
+                        Password = "crystalpassword"
+                    };
+                    Insert(user);
+                    user = new User
+                    {
+                        UserName = "athompson",
+                        Password = "abbypassword"
+                    };
+                    Insert(user);
+                    user = new User
+                    {
+                        UserName = "kxiong",
+                        Password = "kathypassword"
+                    };
+                    Insert(user);
+                }
+            }
+        }
+
+        public static bool Login(User user)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(user.UserName))
+                {
+                    if (!string.IsNullOrEmpty(user.Password))
+                    {
+                        using (SpookyWisconsinEntities dc = new SpookyWisconsinEntities())
+                        {
+                            tblUser tblUser = dc.tblUsers.FirstOrDefault(u => u.Username == user.UserName);
+
+                            if (tblUser != null)
+                            {
+                                if (tblUser.Password == GetHash(user.Password))
+                                {
+                                    //Valid Login
+                                    user.Id = tblUser.Id;
+                                    return true;
+                                }
+                                else
+                                {
+                                    throw new LoginFailureException();
+                                }
+                            }
+                            else
+                            {
+                                throw new LoginFailureException("User ID not found.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new LoginFailureException("Password was not set.");
+                    }
+                }
+                else
+                {
+                    throw new LoginFailureException("UserID was not set.");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
 
         public static int Insert(User user, bool rollback = false)
         {
@@ -28,12 +130,13 @@ namespace SDG.SpookyWisconsin.BL
                     if (rollback) { dbContextTransaction = dc.Database.BeginTransaction(); }
 
                     tblUser row = new tblUser();
-                    //Fill the table - TODO: Fill in other columns when the database is connected
+                    //Fill the table
                     row.Id = new Guid();
-
+                    row.Username = user.UserName;
+                    row.Password = user.Password;
 
                     user.Id = row.Id;
-                    dc.tblUser.Add(row);
+                    dc.tblUsers.Add(row);
                     results = dc.SaveChanges();
 
                     if (rollback) dbContextTransaction.Rollback();
@@ -57,11 +160,10 @@ namespace SDG.SpookyWisconsin.BL
                     IDbContextTransaction dbContextTransaction = null;
                     if (rollback) { dbContextTransaction = dc.Database.BeginTransaction(); }
 
-                    tblUser row = dc.tblUser.FirstOrDefault(d => d.Id == user.Id);
-
-                    //TODO: Fill in the updated fields once the database is completed
-                    //ex: row.State = user.State...
-
+                    tblUser row = dc.tblUsers.FirstOrDefault(d => d.Id == user.Id);
+                    
+                    row.Username = user.UserName;
+                    row.Password = user.Password;
 
                     results = dc.SaveChanges();
 
@@ -82,12 +184,13 @@ namespace SDG.SpookyWisconsin.BL
             {
                 using (SpookyWisconsinEntities dc = new SpookyWisconsinEntities())
                 {
-                    var row = (from pd in dc.tblUser
+                    var row = (from pd in dc.tblUsers
                                where pd.Id == id
                                select new
                                {
                                    Id = pd.Id,
-                                   //TODO - Joins and other fields
+                                   UserName = pd.Username,
+                                   Password = pd.Password
 
                                }).FirstOrDefault();
                     if (row != null)
@@ -95,7 +198,8 @@ namespace SDG.SpookyWisconsin.BL
                         return new User
                         {
                             Id = row.Id,
-                            ///TODO - Joins and other fields
+                            UserName = row.UserName,
+                            Password = row.Password
                         };
                     }
                     else
@@ -116,18 +220,19 @@ namespace SDG.SpookyWisconsin.BL
 
             using (SpookyWisconsinEntities dc = new SpookyWisconsinEntities())
             {
-                var useres = (from pd in dc.tblUser
-                                      orderby pd.FirstName
+                var useres = (from pd in dc.tblUsers
+                                      orderby pd.Username
                                       select new
                                       {
                                           Id = pd.Id,
-                                          //TODO - Joins and other fields
-
+                                          UserName = pd.Username,
+                                          Password = pd.Password
                                       }).ToList();
                 useres.ForEach(pd => rows.Add(new User
                 {
                     Id = pd.Id,
-                    //TODO - Joins and other fields
+                    Password = pd.Password,
+                    UserName = pd.UserName
                 }));
             }
             return rows;
@@ -143,9 +248,9 @@ namespace SDG.SpookyWisconsin.BL
                     IDbContextTransaction dbContextTransaction = null;
                     if (rollback) { dbContextTransaction = dc.Database.BeginTransaction(); }
 
-                    tblUser row = dc.tblUser.FirstOrDefault(d => d.Id == id);
+                    tblUser row = dc.tblUsers.FirstOrDefault(d => d.Id == id);
 
-                    dc.tblUser.Remove(row);
+                    dc.tblUsers.Remove(row);
                     results = dc.SaveChanges();
 
                     if (rollback) dbContextTransaction.Rollback();
