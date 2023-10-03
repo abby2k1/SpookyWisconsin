f41a8fb6058385a3689708555ccbf780d580d994
Cadin Marohl <300071425@fvtc.edu>
2023-10-03 13:49:18 -0500

    1. Fixed details on Layout Page 2. Added Sessions to UI 3. updated UserManager to work with login

---
+++ SDG.SpookyWisconsin.WebUI/Controllers/UserController.cs
@@ -0,0 +1,143 @@
+﻿using SDG.SpookyWisconsin.BL.Models;
+using SDG.SpookyWisconsin.BL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+
+namespace SDG.SpookyWisconsin.WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public void SetUser(User user)
+        {
+            HttpContext.Session.SetObject("user", user);
+            if (user != null)
+            {
+                HttpContext.Session.SetObject("fullname", "Welcome " + user.UserName);
+            }
+            else
+            {
+                HttpContext.Session.SetObject("fullname", string.Empty);
+            }
+        }
+        
+
+
+        public IActionResult Logout()
+        {   
+            SetUser(null);
+            return View();
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            UserManager.Seed();
+            TempData["returnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            try
+            {
+                bool result = UserManager.Login(user);
+                SetUser(user);
+
+                if (TempData["returnUrl"] != null)
+                    return Redirect(TempData["returnUrl"]?.ToString());
+                else
+                    return RedirectToAction(nameof(Index), "Movie");
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Error = ex.Message;
+                return View(user);
+            }
+        }
+
+        // GET: UserController
+        public ActionResult Index()
+        {
+            return View(UserManager.Load());
+        }
+
+        // GET: UserController
+        public ActionResult Seed()
+        {
+            UserManager.Seed();
+            return View();
+        }
+
+        // GET: UserController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
