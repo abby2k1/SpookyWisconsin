cf31ac4bf045c83a25d9fd9dad2a9792541ca75e
Kathy Xiong <700201382@fvtc.edu>
2023-10-03 00:55:43 -0500

    Created test classes for BL Models

---
+++ SDG.SpookyWisconsin.BL.Test/utCustomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDG.SpookyWisconsin.BL.Test
+{
+    public class utCustomer
+    {
+
+    }
+}
