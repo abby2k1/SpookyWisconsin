8a832d630bd54d26b8cadc7c8c50929b00f1d97b
Cadin Marohl <300071425@fvtc.edu>
2023-10-18 14:30:40 -0500

    Added about and contact pages. Also added them to Layout Nav bar.

--- SDG.SpookyWisconsin.WebUI/Controllers/HomeController.cs
+++ SDG.SpookyWisconsin.WebUI/Controllers/HomeController.cs
@@ -22,6 +22,14 @@ namespace SDG.SpookyWisconsin.WebUI.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
