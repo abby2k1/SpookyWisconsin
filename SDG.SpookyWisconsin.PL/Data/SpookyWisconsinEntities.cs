4a7c48cdb4c35f969f09907d86b5e31b3e4efc96
Thay Lee <500160916@fvtc.edu>
2023-10-24 10:20:46 -0500

    PL-Rework to data first. Running into problems, asking group.

--- SDG.SpookyWisconsin.PL/Data/SpookyWisconsinEntities.cs
+++ SDG.SpookyWisconsin.PL/Data/SpookyWisconsinEntities.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SDG.SpookyWisconsin.PL.Entities;
 
 namespace SDG.SpookyWisconsin.PL;
@@ -39,4 +34,19 @@ public partial class SpookyWisconsinEntities : DbContext
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SDG.SpookyWisconsin.DB;Integrated Security=True");
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<tblAddress>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblAddre__");
+
+            entity.ToTable("tblAddress");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Street)
+            .HasMaxLength(50)
+            .IsUnicode(false);
+
+        });
+    }
 }
