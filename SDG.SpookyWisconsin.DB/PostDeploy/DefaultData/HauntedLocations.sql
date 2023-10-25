5edf0de6926792b5f697de5581122ad2f764d059
Crystal Perez <300075558@fvtc.edu>
2023-10-25 12:23:07 -0500

    Removed County Id from Haunted Location.

--- SDG.SpookyWisconsin.DB/PostDeploy/DefaultData/HauntedLocations.sql
+++ SDG.SpookyWisconsin.DB/PostDeploy/DefaultData/HauntedLocations.sql
@@ -1,32 +1,30 @@
 ﻿BEGIN
     DECLARE @AddressId uniqueidentifier;
-    DECLARE @CountyId uniqueidentifier;
     SELECT @AddressId = Id FROM tblAddress WHERE Street = '6517 Lynch Bridge Rd';
-    SELECT @CountyId = Id FROM tblCounty WHERE Name = 'Dane';
-	INSERT INTO tblHauntedLocation (Id, AddressId, CountyId, Name)
+	INSERT INTO tblHauntedLocation (Id, AddressId, Name)
 	VALUES
-	(NEWID(), 1, 1, 'Siren Bridge'),
-	(NEWID(), 2, 2, 'Clark County Insane Asylum'),
-	(NEWID(), 3, 3, 'Sanatorium Hill'),
-	(NEWID(), 4, 3, 'Forest Hill Cemetery'),
-	(NEWID(), 5, 4, 'Nelsen Hall'),
-	(NEWID(), 6, 5, 'Witch Road'),
-	(NEWID(), 7, 5, 'Octagon House'),
-	(NEWID(), 8, 6, 'Dartford Cemetery'),
-	(NEWID(), 9, 7, 'City of Black River Falls'),
-	(NEWID(), 10, 8, 'Hotel Hell'),
-	(NEWID(), 11, 9, 'The Pfister Hotel'),
-	(NEWID(), 12, 9, 'Ambassador Hotel'),
-	(NEWID(), 13, 9, 'Morris Pratt Institute'),
-	(NEWID(), 14, 9, 'Riverside Theater'),
-	(NEWID(), 15, 10, 'Riverside Cemetery'),
-	(NEWID(), 16, 11, 'Boy Scout Lane'),
-	(NEWID(), 17, 11, 'Bloody Bride Bridge'),
-	(NEWID(), 18, 12, 'Al Ringling Mansion'),
-	(NEWID(), 19, 12, 'Old Baraboo Inn'),
-	(NEWID(), 20, 13, 'Summerwind Mansion'),
-	(NEWID(), 21, 14, 'Plainfield Cemetery'),
-	(NEWID(), 22, 15, 'The Grand Opera House'),
-	(NEWID(), 23, 15, 'Winnebago Mental Health Institute'),
-	(NEWID(), 24, 16, 'Wood County Insane Asylum')
+	(NEWID(), 1, 'Siren Bridge'),
+	(NEWID(), 2, 'Clark County Insane Asylum'),
+	(NEWID(), 3, 'Sanatorium Hill'),
+	(NEWID(), 4, 'Forest Hill Cemetery'),
+	(NEWID(), 5, 'Nelsen Hall'),
+	(NEWID(), 6, 'Witch Road'),
+	(NEWID(), 7, 'Octagon House'),
+	(NEWID(), 8, 'Dartford Cemetery'),
+	(NEWID(), 9, 'City of Black River Falls'),
+	(NEWID(), 10, 'Hotel Hell'),
+	(NEWID(), 11, 'The Pfister Hotel'),
+	(NEWID(), 12, 'Ambassador Hotel'),
+	(NEWID(), 13, 'Morris Pratt Institute'),
+	(NEWID(), 14, 'Riverside Theater'),
+	(NEWID(), 15, 'Riverside Cemetery'),
+	(NEWID(), 16, 'Boy Scout Lane'),
+	(NEWID(), 17, 'Bloody Bride Bridge'),
+	(NEWID(), 18, 'Al Ringling Mansion'),
+	(NEWID(), 19, 'Old Baraboo Inn'),
+	(NEWID(), 20, 'Summerwind Mansion'),
+	(NEWID(), 21, 'Plainfield Cemetery'),
+	(NEWID(), 22, 'The Grand Opera House'),
+	(NEWID(), 23, 'Winnebago Mental Health Institute'),
+	(NEWID(), 24, 'Wood County Insane Asylum')
 END
