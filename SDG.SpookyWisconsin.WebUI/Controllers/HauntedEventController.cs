4f80ba31a8e06f1a7c5caf713c5412c0b8d1d4ac
Crystal Perez <300075558@fvtc.edu>
2023-10-25 00:17:25 -0500

    Added Controllers for Haunted Events and Locations

---
+++ SDG.SpookyWisconsin.WebUI/Controllers/HauntedEventController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SDG.SpookyWisconsin.BL;
+
+namespace SDG.SpookyWisconsin.WebUI.Controllers
+{
+    public class HauntedEventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(HauntedEventManager.Load());
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BL.Models.HauntedEvent hauntedEvent)
+        {
+            try
+            {
+                HauntedEventManager.Insert(hauntedEvent);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, BL.Models.HauntedEvent hauntedEvent)
+        {
+            try
+            {
+                HauntedEventManager.Update(hauntedEvent);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, BL.Models.HauntedEvent hauntedEvent)
+        {
+            try
+            {
+                //HauntedEventManager.Delete();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SDG.SpookyWisconsin.WebUI/Controllers/HauntedLocationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SDG.SpookyWisconsin.BL;
+
+namespace SDG.SpookyWisconsin.WebUI.Controllers
+{
+    public class HauntedLocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(HauntedLocationManager.Load());
+        }
+
+        public ActionResult Details(int id, BL.Models.HauntedLocation hauntedLocation)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
