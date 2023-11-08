ce128b6600d7acf6baccbf1f11da4579b0ab98fa
Thay Lee <500160916@fvtc.edu>
2023-11-07 19:39:59 -0600

    Added newsletter and customer controller and views.

--- SDG.SpookyWisconsin.WebUI/Controllers/CustomerController.cs
+++ SDG.SpookyWisconsin.WebUI/Controllers/CustomerController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using SDG.SpookyWisconsin.BL;
+using SDG.SpookyWisconsin.WebUI.Models;
 
 namespace SDG.SpookyWisconsin.WebUI.Controllers
 {
@@ -7,6 +10,86 @@ namespace SDG.SpookyWisconsin.WebUI.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Details(Guid id)
+        {
+            return View(CustomerManager.LoadById(id));
+        }
+
+        public ActionResult Create()
+        {
+            if (Authenticate.IsAuthenticated(HttpContext))
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "User", new { returnUrl = UriHelper.GetDisplayUrl(HttpContext.Request) });
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BL.Models.Customer customer, bool rollback = false)
+        {
+            try
+            {
+                CustomerManager.Insert(customer, rollback);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit()
+        {
+            if (Authenticate.IsAuthenticated(HttpContext))
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "User", new { returnUrl = UriHelper.GetDisplayUrl(HttpContext.Request) });
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Guid id, BL.Models.Customer customer, bool rollback = false)
+        {
+            try
+            {
+                CustomerManager.Update(customer, rollback);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+
+        public ActionResult Delete(Guid id)
+        {
+            return View(CustomerManager.LoadById(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Guid id, BL.Models.Customer customer, bool rollback = false)
+        {
+            try
+            {
+                CustomerManager.Delete(id, rollback);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
--- SDG.SpookyWisconsin.WebUI/Controllers/NewsLetterController.cs
+++ SDG.SpookyWisconsin.WebUI/Controllers/NewsLetterController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using SDG.SpookyWisconsin.BL;
+using SDG.SpookyWisconsin.WebUI.Models;
 
 namespace SDG.SpookyWisconsin.WebUI.Controllers
 {
@@ -6,7 +9,87 @@ namespace SDG.SpookyWisconsin.WebUI.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(NewsLetterManager.Load());
+        }
+
+        public ActionResult Details(Guid id)
+        {
+            return View(NewsLetterManager.LoadById(id));
+        }
+
+        public ActionResult Create()
+        {
+            if (Authenticate.IsAuthenticated(HttpContext))
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "User", new { returnUrl = UriHelper.GetDisplayUrl(HttpContext.Request) });
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BL.Models.NewsLetter newsLetter, bool rollback = false)
+        {
+            try
+            {
+                NewsLetterManager.Insert(newsLetter, rollback);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit()
+        {
+            if (Authenticate.IsAuthenticated(HttpContext))
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "User", new { returnUrl = UriHelper.GetDisplayUrl(HttpContext.Request) });
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Guid id, BL.Models.NewsLetter newsLetter, bool rollback = false)
+        {
+            try
+            {
+                NewsLetterManager.Update(newsLetter, rollback);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+
+        public ActionResult Delete(Guid id)
+        {
+            return View(NewsLetterManager.LoadById(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Guid id, BL.Models.NewsLetter newsLetter, bool rollback = false)
+        {
+            try
+            {
+                NewsLetterManager.Delete(id, rollback);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
