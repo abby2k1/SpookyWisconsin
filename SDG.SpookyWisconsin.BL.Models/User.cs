89ea0b7cc0653b8c6b3e0d5cf8bcac8bff60a44d
Abigail Thompson <mail@abigailthompson.net>
2023-11-20 21:35:50 -0600

    find and replace UserName -> Username

--- SDG.SpookyWisconsin.BL.Models/User.cs
+++ SDG.SpookyWisconsin.BL.Models/User.cs
@@ -11,7 +11,7 @@ namespace SDG.SpookyWisconsin.BL.Models
     {
         public Guid Id { get; set; }
         [DisplayName("Username")]
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
     }
 }
