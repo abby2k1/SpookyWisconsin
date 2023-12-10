c5d9b14b68f423aeb165c1c63325cdc421be71d3
Crystal Perez <300075558@fvtc.edu>
2023-12-09 18:52:47 -0600

    Updated merch items in order item table

--- SDG.SpookyWisconsin.DB/PostDeploy/DefaultData/OrderItem.sql
+++ SDG.SpookyWisconsin.DB/PostDeploy/DefaultData/OrderItem.sql
@@ -2,7 +2,7 @@
 DECLARE @OrderId uniqueidentifier
 DECLARE @MerchId1 uniqueidentifier
 SELECT @OrderId = Id FROM tblOrder WHERE OrderDate = '11-02-2023'
-SELECT @MerchId1 = Id FROM tblMerch WHERE MerchName = 'Spooky Wisconsin Long Sleeve Shirt'
+SELECT @MerchId1 = Id FROM tblMerch WHERE MerchName = 'Spooky Wisconsin Short Sleeve Shirt'
 INSERT INTO tblOrderItem (Id, OrderId, MerchId, Quantity, Cost)
 VALUES
 (NEWID(), @OrderId, @MerchId1, 1, 1.50)
@@ -12,16 +12,16 @@ INSERT INTO tblOrderItem (Id, OrderId, MerchId, Quantity, Cost)
 VALUES
 (NEWID(), @OrderId, @MerchId1, 1, 0.85)
 SELECT @OrderId = Id FROM tblOrder WHERE OrderDate = '11-16-2023'
-SELECT @MerchId1 = Id FROM tblMerch WHERE MerchName = 'Ghoul Shirt'
+SELECT @MerchId1 = Id FROM tblMerch WHERE MerchName = 'Spooky Wisconsin Tumbler'
 INSERT INTO tblOrderItem (Id, OrderId, MerchId, Quantity, Cost)
 VALUES
 (NEWID(), @OrderId, @MerchId1, 1, 9.75)
 SELECT @OrderId = Id FROM tblOrder WHERE OrderDate = '12-01-2023'
-SELECT @MerchId1 = Id FROM tblMerch WHERE MerchName = 'Haunted House Sticker'
+SELECT @MerchId1 = Id FROM tblMerch WHERE MerchName = 'Spooky Wisconsin Tote'
 INSERT INTO tblOrderItem (Id, OrderId, MerchId, Quantity, Cost)
 VALUES
 (NEWID(), @OrderId, @MerchId1, 1, 96.96)
-SELECT @MerchId1 = Id FROM tblMerch WHERE MerchName = 'Spooky Wisconsin Long Sleeve Shirt'
+SELECT @MerchId1 = Id FROM tblMerch WHERE MerchName = 'Spooky Wisconsin Short Sleeve Shirt'
 INSERT INTO tblOrderItem (Id, OrderId, MerchId, Quantity, Cost)
 VALUES
 (NEWID(), @OrderId, @MerchId1, 2, 400.35)
