ff40a8e669a9ab8ed82f7154e7171a1d1eb91694
Crystal Perez <300075558@fvtc.edu>
2023-12-09 19:11:53 -0600

    Edited titles for pages and edited image path for haunted event.

--- SDG.SpookyWisconsin.DB/PostDeploy/DefaultData/HauntedEvent.sql
+++ SDG.SpookyWisconsin.DB/PostDeploy/DefaultData/HauntedEvent.sql
@@ -3,20 +3,20 @@
 	SELECT @HauntedLocationId = Id FROM tblHauntedLocation WHERE Name = 'Forest Hill Cemetery'
 	INSERT INTO tblHauntedEvent (Id, HauntedLocationId, Name, Date, Description, ImagePath)
 	VALUES
-	(NEWID(), @HauntedLocationId, 'Veteran Walk-Through in Forest Hill Cemetery', '10-31-2024', 'Veteran Walk-Through in Forest Hill Cemetery', '../images/locations/foresthill2.png')
+	(NEWID(), @HauntedLocationId, 'Veteran Walk-Through in Forest Hill Cemetery', '10-31-2024', 'Veteran Walk-Through in Forest Hill Cemetery', 'foresthill2.png')
 
 	SELECT @HauntedLocationId = Id FROM tblHauntedLocation WHERE Name = 'Riverside Cemetery'
 	INSERT INTO tblHauntedEvent (Id, HauntedLocationId, Name, Date, Description, ImagePath)
 	VALUES
-	(NEWID(), @HauntedLocationId, 'Kate Blood Gravestone Story Time', '11-05-2024', 'Kate Blood Gravestone Story Time', '../images/locations/riversidecemetery2.png')
+	(NEWID(), @HauntedLocationId, 'Kate Blood Gravestone Story Time', '11-05-2024', 'Kate Blood Gravestone Story Time', 'riversidecemetery2.png')
 
 	SELECT @HauntedLocationId = Id FROM tblHauntedLocation WHERE Name = 'Al Ringling Mansion'
 	INSERT INTO tblHauntedEvent (Id, HauntedLocationId, Name, Date, Description, ImagePath)
 	VALUES
-	(NEWID(), @HauntedLocationId, 'Keeping Up with the Circus', '11-16-2024', 'Keeping Up with the Circus', '../images/locations/alringling1.png')
+	(NEWID(), @HauntedLocationId, 'Keeping Up with the Circus', '11-16-2024', 'Keeping Up with the Circus', 'alringling1.png')
 
 	SELECT @HauntedLocationId = Id FROM tblHauntedLocation WHERE Name = 'Old Baraboo Inn'
 	INSERT INTO tblHauntedEvent (Id, HauntedLocationId, Name, Date, Description, ImagePath)
 	VALUES
-	(NEWID(), @HauntedLocationId, 'Stay the Night for a Fright', '12-04-2024', 'Stay the Night for a Fright', '../images/locations/oldbaraboo1.png')
+	(NEWID(), @HauntedLocationId, 'Stay the Night for a Fright', '12-04-2024', 'Stay the Night for a Fright', 'oldbaraboo1.png')
 END
